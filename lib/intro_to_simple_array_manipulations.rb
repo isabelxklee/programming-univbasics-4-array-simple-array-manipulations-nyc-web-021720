8aa816c001869a450f0fc744867bd220c0ab5364
isabel k. lee <hello@isabelklee.co>
2020-01-26 21:22:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def pop_with_args(array)
   array.pop[3][2]
-  array.pop(array.length-2)
+#  array.pop(array.length-2)
 end
 
-@dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+#@dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
